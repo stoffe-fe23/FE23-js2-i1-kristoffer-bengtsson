2ff4623a9f5cc880821632ef400d6a36c55a9e66
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-02-12 10:33:35 +0100

    todo

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 import gameInterface from "./modules/GameInterface.js";
 /*
     TODO-lista: 
-    * Bättre feedback / visuell presentation av vad som händer i spelet.
-    * Ordentlig Game Over-skärm och presentation av att en match är över. 
-    * Bättre tooltips för skills (istf. title-attribut som nu).
-    * Visad textbeskrivning av vald klass i character creatorn.
+    * Bättre feedback / visuell presentation av vad som händer i spelet. (GameInterface.js: rad 112)
+    * Ordentlig Game Over-skärm och presentation av att en match är över. (GameInterface.js: rad 139)
+    * Bättre tooltips för skill-beskrivningar istf. title-attribut som nu. (Game.js: rad 149)
+    * Visad textbeskrivning av vald klass i character creatorn?
     * Utseende / design.
 */
 
